00bf87fefa92fbc6ad8ab3de0afe4c732b3957b7
renanmurilo <renanmurilosantosg@gmail.com>
2019-05-12 14:52:24 -0300

    ajustes finais

---
+++ src/assets/js/scroll.js
@@ -0,0 +1,21 @@
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'anime';
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeigth * 3) / 4);
+    target.forEach(function(element){
+        if((windowTop) > element.offsetTop){
+            element.classList.add(animationClass);
+        }else{
+            element.classList.remove(animationClass);
+        }
+    })
+}
+
+if(target.length) {
+
+    window.addEventListener('scroll', function(){
+        animeScroll();
+        console.log();
+    })
+}
